8545b236809a388b1249ad0d408a362f2209c4c4
the-customer <niangalytall@gmail.com>
2019-02-06 21:26:44 +0000

    fist commit

---
+++ src/main/java/com/thecustomer/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,14 @@
+package com.thecustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+
+}
+
